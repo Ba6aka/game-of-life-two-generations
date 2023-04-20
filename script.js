f7b38d8ba081cdd9f4b6b2d5ea62dbdead72035f
Ba6aka <bam6ykba6aka@gmail.com>
2023-04-20 10:07:35 +0300

    Add fuction checkState

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const canvasParent = document.querySelector('main')
 const rowLength = window.innerWidth / cellSize.value | 0
 const rowCount = (window.innerHeight - 150) / cellSize.value | 0
 
+let blueState = document.querySelector('.blue')
+let blackState = document.querySelector('.black')
+let countOfBlack = document.querySelector('.black-count')
+let countOfBlue = document.querySelector('.blue-count')
 let firstState = ['3,3', '2,3', '3,2', '3,1', '1,2', '38,37', '39,35', '38,36', '40,36', '40,37', '39,38']
 let secondState = ['20,20', '21,21', '22,20', '22,19', '23,22', '23,23', '27,17', '18,18', '15,17', '17,15', '15,15', '32,18']
 
@@ -39,3 +43,13 @@ function createGrid(rowCount, rowLength, state, secondState) {
         }
     }
 }
+
+
+function checkState() {
+    if (blackState.checked) {
+        return firstState
+    } else if (blueState.checked) {
+        return secondState
+    }
+}
+
